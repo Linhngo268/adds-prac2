01926c5abe75df294b76103aeef29cd62d23cbae
Linhngo268 <83965979+Linhngo268@users.noreply.github.com>
2022-08-04 17:40:08 +0930

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -1,18 +1,26 @@
 #include "Referee.h"
+#include "Computer.h"
+#include "Human.h"
 Referee::Referee(){
 
 }
 char Referee::RefGame(Human player1, Computer player2){
     char playerHuman=player1.makeMove();
     char computermove=player2.makeMove();
-if(playerHuman == 'R' && computermove =='R'){
-    return 'T';
-}
-else if(playerHuman=='P' && computermove =='R'){
-    return 'W';
-}
-else if (playerHuman=='S' && computermove =='R'){
-    return 'L';
-}
-return 0;
+if(playerHuman == 'R' && computermove == 'S'){
+        return 'W';
+    }else if(playerHuman == 'R' && computermove == 'P'){
+        return 'L';
+    }else if(playerHuman == 'S' && computermove == 'P'){
+        return 'W';
+    }else if(playerHuman == 'S' && computermove == 'R'){
+        return 'L';
+    }else if(playerHuman == 'P' && computermove == 'R'){
+        return 'W';
+    }else if(playerHuman == 'P' && computermove == 'S'){
+        return 'L';
+    }
+    else {
+        return 'T';
+    }
 }
